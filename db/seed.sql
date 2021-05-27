6dca05ff470a5749b4fdc892f7bd5fec61e54060
Danny Andrews <lunde.andrews@gmail.com>
2021-05-27 10:46:36 -0500

    Working routes

---
+++ db/seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO pets (name, age) VALUES ('Alice', 30);
+INSERT INTO pets (name, age) VALUES ('Morgan', 1);
--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const morgan = require('morgan');
+const { Pool } = require('pg')
 
 const server = express();
+
+const pool = new Pool({
+  connectionString: 'postgres://localhost:5432/temp'
+});
 
 server.use(morgan('tiny'));
 server.use(express.json());
 
 server.get('/api/pets', (req, res) => {
-  res.send('made request to /api/pes');
+  pool.query('SELECT * FROM pets', (err, data) => {
+    res.json(data.rows);
+  });
+});
+
+// Request Body -> req.body
+// Headers -> req.get
+// Request Path/params -> req.params
+// Request Query -> req.query
+//    GET http://localhost:3000/api/posts?search=programming&limit=100
+
+server.post('/api/pets', (req, res) => {
+  const { name, age } = req.body;
+
+  pool.query('INSERT INTO pets (name, age) VALUES ($1, $2);', [name, age], (err, data) => {
+    console.log('err', err);
+    console.log(data.rows);
+    res.json({ name, age });
+  });
 });
 
 server.listen(3000, () => {
